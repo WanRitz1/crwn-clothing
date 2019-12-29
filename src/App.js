ffc54760d13e0354fc7df19d5ca882e70d71af2f
WanRitz1 <56873168+WanRitz1@users.noreply.github.com>
2019-12-30 02:39:37 +0800

    create menu item to navigate withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import HomePage from './pages/Homepage/homepage.component';
 
-const HomePage = props => {
-  console.log(props);
-  return (
-    <div>
-      <button onClick={() => props.history.push('/topics')}>Topics </button>
-      <h1>HOME PAGE</h1>
-    </div>
-  );
-};
 
-const TopicsList = props => {
-  return (
-    <div>
-      <h1>TOPIC LIST PAGE</h1>
-      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
-      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
-    </div>
-  );
-};
+const Hatpage=() => (
+ <div>
+ </div>
 
-const TopicDetail = props => {
-  return (
-    <div>
-      <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
-    </div>
-  );
-};
+);
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/blog/asdqw/topics' component={TopicsList} />
-      <Route path='/blog/asdqw/topics/:topicId' component={TopicDetail} />
-      <Route exact path='/blog/topics' component={TopicsList} />
-      <Route path='/blog/topics/:topicId' component={TopicDetail} />
+      <Switch>
+     <Route exact path='/' component = {HomePage}/>
+     <Route path='/hats' component = {Hatpage}/>
+     </Switch>
     </div>
   );
 }
