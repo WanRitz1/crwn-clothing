41c48a29a9a8b6bb377bc16190e54e3cff4c3b6e
WanRitz1 <56873168+WanRitz1@users.noreply.github.com>
2020-01-10 03:14:42 +0800

    collection part 2

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SHOP_DATA from './shop.data.js';
-
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 class ShopPage extends React.Component {
         constructor (props){
             super (props);
@@ -10,8 +10,15 @@ class ShopPage extends React.Component {
                 };
         }
 render(){
-    return<div>Shop Page</div>;
-}
+    const {collections} = this.state;
+    return(<div className = 'Shop Page'>
+        {
+            collections.map(({id,...otherCollectionProps  }) => (
+                <CollectionPreview key={id}{...otherCollectionProps}/>
+            )) 
+        }
+    </div>);
+    }
 
 }
 export default ShopPage;
