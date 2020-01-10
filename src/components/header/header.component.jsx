77fe2a8b882b777bbb9c57fe3963ffe2b3b32098
WanRitz1 <56873168+WanRitz1@users.noreply.github.com>
2020-01-10 16:58:36 +0800

    header 2

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React  from 'react';
 import './header.styles.scss';
 import {Link} from 'react-router-dom';
-import {ReactComponent as Logo} from '../../assets/crown.svg';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 const Header = () =>(
     <div className='header'>
-        <Link className = 'logo-container' to="/">
+        <Link className ='logo-container' to='/'>
             <Logo className = 'logo'/>
         </Link>
         <div className='options'>
