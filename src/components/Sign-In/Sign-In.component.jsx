79639af56b17fc96bcc63d16c45a74a8e8c27a9e
WanRitz1 <56873168+WanRitz1@users.noreply.github.com>
2020-01-21 10:44:57 +0800

    integrated with firebase create sign in and sign in with google

--- src/components/Sign-In/Sign-In.component.jsx
+++ src/components/Sign-In/Sign-In.component.jsx
@@ -39,9 +39,12 @@ class SignIn extends React.Component{
                     handleChange={this.handleChange} 
                     label = 'Password'required/>
                    
-                    
+                    <div className ='buttons'>
                     <CustomButton type ='submit'>Sign In</CustomButton>
-    <CustomButton onClick = {signInWithGoogle}>{''}Sign In With Google{''}</CustomButton>
+                    <CustomButton onClick = {signInWithGoogle} isGoogleSignIn > 
+                    Sign In With Google
+                    </CustomButton>
+                    </div>
                 </form>
             </div>
         )
