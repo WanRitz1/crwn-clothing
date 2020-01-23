6277e43185997015338db8362545ef7da83b0abb
WanRitz1 <56873168+WanRitz1@users.noreply.github.com>
2020-01-23 21:06:55 +0800

    store data in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { useRef } from 'react';
 
 const config = {
     
@@ -13,6 +14,33 @@ const config = {
         appId: "1:462257542845:web:641bd4c754808dad73c9d4",
         measurementId: "G-RDS9W4JK9T"
 }
+
+export const createUserProfileDocument= async(userAuth, additionalData) => {
+        if(!userAuth) return;
+       // console.log(firestore.doc('users/123sdbfksbfkb'));
+        const userRef=firestore.doc(`users/${userAuth.uid}`);
+        const snapShot = await userRef.get();
+        console.log(snapShot);
+
+        if(!snapShot.exists){
+                const {displayName,email} = userAuth;
+                const createdAt = new Date();
+
+                try{
+                   await userRef.set({
+                           displayName, email,createdAt, ...additionalData
+                   })          
+                } catch (error){
+                                console.log('error creating user', error.message);
+                }
+                
+        }
+        return userRef;
+};
+
+
+
+
 firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
