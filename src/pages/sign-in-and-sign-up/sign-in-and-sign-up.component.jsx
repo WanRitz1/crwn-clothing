769ad9278b535c862954ff3e0f589090956f8684
WanRitz1 <56873168+WanRitz1@users.noreply.github.com>
2020-01-25 10:10:02 +0800

    sign up with email and password

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
 import SignIn from '../../components/Sign-In/Sign-In.component';
+import SignUp from '../../components/Sign-Up/Sign-Up.component';
 
 const SignInAndSignUp = () => (
     <div className='Sign-In-And-Sign-Up'>
         <SignIn/>
-
+        <SignUp/>
     </div>
 
 );
