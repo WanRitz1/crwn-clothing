dcbfa12ed09b8622aec446ff207ee16a5ba6c73c
WanRitz1 <56873168+WanRitz1@users.noreply.github.com>
2020-02-13 11:01:18 +0800

    svg shopping bag icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,14 +3,14 @@ import './header.styles.scss';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {auth} from '../../firebase/firebase.utils';
-//import {ReactComponet as Logo}  from '../../image';
+import {ReactComponent as Logo}  from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
 
 
 const Header = ({currentUser}) =>(
     <div className='header'>
         <Link className ='logo-container' to='/'>
-          
+          <Logo className='Logo'/>
         </Link>
         <div className='options'>
             <Link className = 'option' to='/shop'>
