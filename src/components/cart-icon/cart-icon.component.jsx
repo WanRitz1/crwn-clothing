fcc0b0a09d131394f9d531111cefd549b96a06d8
WanRitz1 <56873168+WanRitz1@users.noreply.github.com>
2020-03-02 00:54:03 +0800

    memoization with selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import {connect} from 'react-redux';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
+
 
 const CartIcon = ({toggleCartHidden,itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
@@ -15,7 +17,7 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    itemCount : cartItems.reduce((accumulatedQuantity,cartItem)=> accumulatedQuantity + cartItem.quantity, 0)
+const mapStateToProps = state => ({
+    itemCount : selectCartItemsCount(state)
 });
 export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
