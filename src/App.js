c7a17aa50f4d6450d98d4b13692dcfa23a4ef8d8
WanRitz1 <56873168+WanRitz1@users.noreply.github.com>
2020-03-13 03:04:05 +0800

    checkout page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HomePage from './pages/Homepage/homepage.component';
 import ShopPage  from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import CheckoutPage from './pages/checkout/checkout.component';
 import {auth,createUserProfileDocument}from './firebase/firebase.utils';
 import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selector';
@@ -46,6 +47,7 @@ return (
       <Switch>
      <Route exact path='/' component = {HomePage}/>
      <Route path='/Shop' component = {ShopPage}/>
+     <Route exact path='/checkout' component = {CheckoutPage}/>
      <Route exact path='/SignIn' render = {() => this.props.currentUser ? ( <Redirect to='/' />) : ( <SignInAndSignUp/> )}/>
      </Switch>
     </div>
