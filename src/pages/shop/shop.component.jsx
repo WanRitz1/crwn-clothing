6761f89121175bb9e0c09c527a211856acf5f2da
WanRitz1 <56873168+WanRitz1@users.noreply.github.com>
2020-04-03 23:29:46 +0800

    nested routing 131

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-
+import {Route} from 'react-router-dom'; 
 //import {connect} from 'react-redux';
 //import {createStructuredSelector} from 'reselect';
 import CollectionsOverview from '../../components/collections-overview/collections-overiew.component';
+import CategoryPage from '../category/category.component';
 //import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 //import {selectCollections} from '../../redux/shop/shop.selectors';
 
-const ShopPage = ({collections}) => (
+const ShopPage = ({match}) => (
 <div className = 'shop-page'>
-<CollectionsOverview/>
+< Route exact path={`${match.path}`} component={CollectionsOverview}/>
+<Route path={`${match.path}/:categoryId`} component={CategoryPage}/>
 </div>  
 );
+    
    
 
   
